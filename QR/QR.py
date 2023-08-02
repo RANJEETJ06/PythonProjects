f76d341da4105837b9dcc88f3f920c57114e92e8
Ranjeet Kumar Jena <ranjeetjena06@gmai.com>
2023-08-02 18:45:39 +0530

    image added

--- QR/QR.py
+++ QR/QR.py
@@ -1,5 +1,5 @@
 import qrcode as qr
 
 image = qr.make(input("Enter the link you want to create QR\n"))
-name = input("Enter a name to give name to qr")
+name = input("Enter a name to give name to qr\n")
 image.save(name + ".png")
